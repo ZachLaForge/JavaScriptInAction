539d13f9680a9797694923e7a289cc3bc64d856c
Zach <zach.laforge@hotmail.com>
2022-03-25 15:26:46 -0400

    sent up initial files and setup + finished problems 1 and 2

---
+++ worksheet.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// #1Days of the Week
+
+let dayOfWeek ="Monday";
+
+console.log(dayOfWeek);
+dayOfWeek ="Friday";
+console.log(`I cant wait for ${dayOfWeek}`)
+
+
+//#2 User Input
+
+let animalInput =prompt("What is you favorite animal?")
+let colorInput =prompt("What is you favorite color?")
+
+console.log(`I've never seen a ${colorInput} ${animalInput} before!`)
