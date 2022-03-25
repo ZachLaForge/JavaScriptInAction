5be09724f772186f233d7d571e7e83eeb213d6a8
Zach <zach.laforge@hotmail.com>
2022-03-25 16:13:37 -0400

    finished problems 3-6 + started on problem 7

--- worksheet.js
+++ worksheet.js
@@ -2,16 +2,87 @@
 
 // #1Days of the Week
 
-let dayOfWeek ="Monday";
+// let dayOfWeek ="Monday";
 
-console.log(dayOfWeek);
-dayOfWeek ="Friday";
-console.log(`I cant wait for ${dayOfWeek}`)
+// console.log(dayOfWeek);
+// dayOfWeek ="Friday";
+// console.log(`I cant wait for ${dayOfWeek}`)
 
 
-//#2 User Input
+// //#2 User Input
 
-let animalInput =prompt("What is you favorite animal?")
-let colorInput =prompt("What is you favorite color?")
+// let animalInput =prompt("What is you favorite animal?")
+// let colorInput =prompt("What is you favorite color?")
 
-console.log(`I've never seen a ${colorInput} ${animalInput} before!`)
+// console.log(`I've never seen a ${colorInput} ${animalInput} before!`)
+
+
+//#3 Meals
+
+// Breakfast - Giant breakfast burrito
+// Lunch - Ramen
+// Dinner - Alfredo
+
+
+// let timeOfDay = "1000";
+
+// let food ="";
+
+// if(timeOfDay < 1200){
+//     food = "Giant breakfast burrito";
+// }
+
+// else if(timeOfDay > 1200 && timeOfDay < 1700) {
+//     food = "Ramen";
+// }
+
+// else if(timeOfDay > 1700) {
+//     food = "Alfredo"
+// }
+
+// console.log(food);
+
+//#5 Random Number
+
+// let randomNumber =Math.floor(Math.random() * 10);
+
+// if(randomNumber === 0 || randomNumber === 1 || randomNumber === 2) {
+//     console.log("Beatles")
+// }
+// if(randomNumber === 0 || randomNumber === 1 || randomNumber === 2) {
+//     console.log("Beatles")
+// }
+
+// else if(randomNumber === 3 || randomNumber === 4 || randomNumber === 5) {
+//     console.log("Stones")
+// }
+
+// else if(randomNumber === 6 || randomNumber === 7 || randomNumber === 8) {
+//     console.log("Floyd")
+// }
+
+// else if(randomNumber === 9 || randomNumber === 10) {
+//     console.log("Hendrix")
+// }
+
+
+// #6 Loops
+
+// for(let i = 0; i < 7; i++) {
+//     console.log("JavaScript is coolio!")
+// }
+
+// for(let i = 0; i < 11; i++) {
+//     console.log(i)
+// }
+
+// for(let i = 0; i < 5; i++) {
+//     console.log("hello")
+//     console.log("goodbye")
+// }
+
+
+//#7 Functions
+
+let favMovie ="Intersterllar";
+    console.log(`${favMovie}`)
